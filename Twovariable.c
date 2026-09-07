9dd540c3b31b6e347f0cabec8f9f4ab1fcefaea9
Jayanth N <jayanthsnr@gmail.com>
2026-09-07 22:10:50 +0530

    7thSep26

---
+++ Twovariable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a ;
+    float b;
+    printf("enter the value a:\n");
+    scanf("%d", &a);
+    printf("Enter the value b:\n");
+    scanf("%f", &b);
+    
+    printf("entered value is %d\n", a);
+    printf("entered value is %f\n", b);
+    
+    return 0;
+}
